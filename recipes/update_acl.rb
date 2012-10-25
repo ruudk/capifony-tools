0231cb6443ec178d6787c8df7e248fc860e1e845
Ruud Kamphuis <ruudk@mphuis.com>
2012-10-25 14:02:53 +0300

    capifony_pretty_print update_acl.rb

--- recipes/update_acl.rb
+++ recipes/update_acl.rb
@@ -2,11 +2,11 @@ after 'symfony:cache:warmup', 'symfony:update_acl'
 
 namespace :symfony do
   task :update_acl, :roles => :app do
-    pretty_print "--> Sets permissions using ACL for app/cache"
+    capifony_pretty_print "--> Sets permissions using ACL for app/cache"
 
     run "cd #{latest_release} && setfacl -R -m u:apache:rwx -m u:$USER:rwx #{app_path + "/cache"}"
     run "cd #{latest_release} && setfacl -dR -m u:apache:rwx -m u:$USER:rwx #{app_path + "/cache"}"
 
-    puts_ok
+    capifony_puts_ok
   end
 end
