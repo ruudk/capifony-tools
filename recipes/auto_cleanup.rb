871bec02fd47de52c55a23e8acfdef81330c9ebe
Ruud Kamphuis <ruudk@mphuis.com>
2012-10-25 14:02:35 +0300

    capifony_pretty_print auto_cleanup.rb

--- recipes/auto_cleanup.rb
+++ recipes/auto_cleanup.rb
@@ -3,10 +3,10 @@ after 'deploy', 'deploy:auto_cleanup'
 
 namespace :deploy do
   task :auto_cleanup, :roles => :app do
-    pretty_print "--> Cleaning up old releases"
+    capifony_pretty_print "--> Cleaning up old releases"
 
     deploy.cleanup
 
-    puts_ok
+    capifony_puts_ok
   end
 end
