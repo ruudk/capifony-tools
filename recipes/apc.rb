88ad28f7425c47f067fcef85234c5e9629d82d9b
Ruud Kamphuis <ruudk@mphuis.com>
2012-10-25 14:02:16 +0300

    capifony_pretty_print apc.rb

--- recipes/apc.rb
+++ recipes/apc.rb
@@ -3,10 +3,10 @@ after 'deploy', 'symfony:apc'
 
 namespace :symfony do
   task :apc, :roles => :app do
-    pretty_print "--> Clearing APC opcode cache"
+    capifony_pretty_print "--> Clearing APC opcode cache"
 
     run "cd #{latest_release} && #{php_bin} #{symfony_console} apc:clear --env=#{symfony_env_prod}"
 
-    puts_ok
+    capifony_puts_ok
   end
 end
