c2cd433e170890c0e3e126c34df462414405ae14
Ruud Kamphuis <ruudk@mphuis.com>
2012-10-25 14:03:13 +0300

    capifony_pretty_print vendors_speedup.rb

--- recipes/vendors_speedup.rb
+++ recipes/vendors_speedup.rb
@@ -3,9 +3,9 @@ before 'symfony:composer:update', 'composer:copy_vendors'
 
 namespace :composer do
   task :copy_vendors, :except => { :no_release => true } do
-    pretty_print "--> Copy vendor file from previous release"
+    capifony_pretty_print "--> Copy vendor file from previous release"
 
     run "vendorDir=#{current_path}/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir #{latest_release}/vendor; fi;"
-    puts_ok
+    capifony_puts_ok
   end
 end
