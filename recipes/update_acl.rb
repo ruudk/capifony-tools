fcf482c77177af297b498fbc3833f6260068e82f
Ruud Kamphuis <ruudk@mphuis.com>
2012-11-02 16:34:48 +0100

    Fix ACL bug

--- recipes/update_acl.rb
+++ recipes/update_acl.rb
@@ -4,8 +4,8 @@ namespace :symfony do
   task :update_acl, :roles => :app do
     capifony_pretty_print "--> Sets permissions using ACL for app/cache"
 
-    run "cd #{latest_release} && setfacl -R -m u:apache:rwx -m u:$USER:rwx #{app_path + "/cache"}"
-    run "cd #{latest_release} && setfacl -dR -m u:apache:rwx -m u:$USER:rwx #{app_path + "/cache"}"
+    run "cd #{latest_release} && setfacl -R -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{app_path + "/cache"}"
+    run "cd #{latest_release} && setfacl -dR -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{app_path + "/cache"}"
 
     capifony_puts_ok
   end
