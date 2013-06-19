968cc29535db55ed9b3f54dfdca8730df8b2c9b8
Ruud Kamphuis <ruudk@mphuis.com>
2013-06-19 22:05:45 +0300

    Update_acl now uses writable_dirs

--- recipes/update_acl.rb
+++ recipes/update_acl.rb
@@ -4,8 +4,8 @@ namespace :symfony do
   task :update_acl, :roles => :app do
     capifony_pretty_print "--> Sets permissions using ACL for app/cache"
 
-    run "cd #{latest_release} && setfacl -R -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{app_path + "/cache"}"
-    run "cd #{latest_release} && setfacl -dR -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{app_path + "/cache"}"
+    run "cd #{latest_release} && setfacl -R -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{writable_dirs.join(" ")}"
+    run "cd #{latest_release} && setfacl -dR -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{writable_dirs.join(" ")}"
 
     capifony_puts_ok
   end
