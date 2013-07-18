6627904ae4c0061efaedc4ba5356093b3c58f8fd
Ruud Kamphuis <ruudk@mphuis.com>
2013-07-18 11:01:29 +0200

    Revert

--- recipes/update_acl.rb
+++ recipes/update_acl.rb
@@ -7,14 +7,13 @@ namespace :symfony do
     dirs = writable_dirs
     dirs.delete(app_path + "/cache")
 
-    run "cd #{latest_release} && setfacl -R -m u:#{webserver_user}:rwX -m u:#{user}:rwX #{dirs.join(" ")}"
-    run "cd #{latest_release} && setfacl -dR -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{dirs.join(" ")}"
+    run "cd #{latest_release} && setfacl -d -P -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{dirs.join(" ")}"
 
     capifony_puts_ok
 
     capifony_pretty_print "--> Sets permissions using ACL for app/cache"
 
-    run "cd #{latest_release} && setfacl -R -m u:#{webserver_user}:rwX -m u:#{user}:rwX #{app_path + "/cache"}"
+    run "cd #{latest_release} && setfacl -R -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{app_path + "/cache"}"
     run "cd #{latest_release} && setfacl -dR -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{app_path + "/cache"}"
 
     capifony_puts_ok
