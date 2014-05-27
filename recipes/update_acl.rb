0ebcab8657bb0c61e57245b6db108b0e66fbc76c
Ruud Kamphuis <ruudk@mphuis.com>
2014-05-27 15:56:53 +0200

    Update update_acl.rb

--- recipes/update_acl.rb
+++ recipes/update_acl.rb
@@ -2,14 +2,17 @@ after 'symfony:cache:warmup', 'symfony:update_acl'
 
 namespace :symfony do
   task :update_acl, :roles => :app do
-    capifony_pretty_print "--> Sets permissions using ACL for writable dirs"
 
     dirs = writable_dirs
     dirs.delete(app_path + "/cache")
 
-    run "cd #{latest_release} && setfacl -d -P -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{dirs.join(" ")}"
+    if not dirs.empty?
+      capifony_pretty_print "--> Sets permissions using ACL for writable dirs"
 
-    capifony_puts_ok
+      run "cd #{latest_release} && setfacl -d -P -m u:#{webserver_user}:rwx -m u:#{user}:rwx #{dirs.join(" ")}"
+
+      capifony_puts_ok
+    end
 
     capifony_pretty_print "--> Sets permissions using ACL for app/cache"
 
